6b30fbfb94db8ab39e6546b21811b25bf5faf20e
David Thomas <david.thomas@leapyear.io>
2018-01-08 16:08:22 -0800

    build with dzslides

--- slides/build.sh
+++ slides/build.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
+
+# format=${1:?format is required as first argument}
+# shift
+
+STARTED=
+
 ls *.c | {
 	while read nextc; do
+    if [ "$STARTED" ]; then
+      echo
+      echo "----"
+      echo
+    fi
+    STARTED=1
     name=$(basename "$nextc" .c)
-    echo "## $name"
     echo "~~~"
     cat "$nextc"
     echo "~~~"
     if [ -f $name.notes ]; then
-      echo ":::"
+      echo "::: notes"
       cat $name.notes
       echo ":::"
     fi
+    if ! gcc -c -o /dev/null $nextc 2>tmp.out; then
+      echo
+      sed 's/^/> /' <tmp.out
+    fi
 	done
-} | pandoc $@ -t s5
+} | pandoc --standalone -t dzslides -o slides.html $@
