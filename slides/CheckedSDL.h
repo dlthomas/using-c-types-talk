c58bb0c9459ebe81826291bb4555e5c4a2e7303e
David Thomas <david.thomas@leapyear.io>
2018-01-09 16:20:21 -0800

    reformat CheckedSDL.h

--- slides/CheckedSDL.h
+++ slides/CheckedSDL.h
@@ -1,4 +1,5 @@
 /* CheckedSDL.h */
+
 #define SDL_CreateTextureFromSurface SDL_CreateTextureFromSurface_Nope
 #define SDL_CreateRenderer SDL_CreateRenderer_Nope
 #define SDL_CreateWindow SDL_CreateWindow_Nope
@@ -20,18 +21,18 @@
 typedef struct main_thread_s { /* int unused; */ } main_thread_t;
 #define TH(THREAD_NAME) THREAD_NAME##_thread_t th __attribute__((unused))
 
-extern DECLSPEC SDL_Window * SDLCALL SDL_CreateWindow(TH(main),
-                                                      const char *title,
-                                                      int x, int y, int w,
-                                                      int h, Uint32 flags);
+extern DECLSPEC SDL_Window * SDLCALL SDL_CreateWindow(
+    TH(main),
+    const char *title, int x, int y, int w, int h, Uint32 flags);
 
 extern DECLSPEC int SDLCALL SDL_PollEvent(TH(main), SDL_Event * event);
 
-extern DECLSPEC SDL_Texture * SDLCALL SDL_CreateTextureFromSurface(TH(main), SDL_Renderer * renderer, SDL_Surface * surface);
+extern DECLSPEC SDL_Texture * SDLCALL SDL_CreateTextureFromSurface(
+    TH(main), SDL_Renderer * renderer, SDL_Surface * surface);
 
-extern DECLSPEC SDL_Renderer * SDLCALL SDL_CreateRenderer(TH(main),
-                                               SDL_Window * window,
-                                               int index, Uint32 flags);
+extern DECLSPEC SDL_Renderer * SDLCALL SDL_CreateRenderer(
+    TH(main),
+    SDL_Window * window, int index, Uint32 flags);
 
 extern DECLSPEC int SDLCALL SDL_Init(TH(main), Uint32 flags);
 
@@ -39,10 +40,9 @@ extern DECLSPEC void SDLCALL SDL_RenderPresent(TH(main), SDL_Renderer * renderer
 
 extern DECLSPEC void SDLCALL SDL_RenderClear(TH(main), SDL_Renderer * renderer);
 
-extern DECLSPEC int SDLCALL SDL_RenderCopy(TH(main),
-                                           SDL_Renderer * renderer,
-                                           SDL_Texture * texture,
-                                           const SDL_Rect * srcrect,
-                                           const SDL_Rect * dstrect);
+extern DECLSPEC int SDLCALL SDL_RenderCopy(
+    TH(main),
+    SDL_Renderer * renderer, SDL_Texture * texture,
+    const SDL_Rect * srcrect, const SDL_Rect * dstrect);
 
 
