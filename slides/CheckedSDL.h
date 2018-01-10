f50851c8672ebd8699f65f401cf6647552f92261
David Thomas <david.thomas@leapyear.io>
2018-01-09 16:31:28 -0800

    clean up CheckedSDL decls

--- slides/CheckedSDL.h
+++ slides/CheckedSDL.h
@@ -21,26 +21,26 @@
 typedef struct main_thread_s { /* int unused; */ } main_thread_t;
 #define TH(THREAD_NAME) THREAD_NAME##_thread_t th __attribute__((unused))
 
-extern DECLSPEC SDL_Window * SDLCALL SDL_CreateWindow(
+SDL_Window * SDL_CreateWindow(
     TH(main),
     const char *title, int x, int y, int w, int h, Uint32 flags);
 
-extern DECLSPEC int SDLCALL SDL_PollEvent(TH(main), SDL_Event * event);
+int SDL_PollEvent(TH(main), SDL_Event * event);
 
-extern DECLSPEC SDL_Texture * SDLCALL SDL_CreateTextureFromSurface(
+SDL_Texture * SDL_CreateTextureFromSurface(
     TH(main), SDL_Renderer * renderer, SDL_Surface * surface);
 
-extern DECLSPEC SDL_Renderer * SDLCALL SDL_CreateRenderer(
+SDL_Renderer * SDL_CreateRenderer(
     TH(main),
     SDL_Window * window, int index, Uint32 flags);
 
-extern DECLSPEC int SDLCALL SDL_Init(TH(main), Uint32 flags);
+int SDL_Init(TH(main), Uint32 flags);
 
-extern DECLSPEC void SDLCALL SDL_RenderPresent(TH(main), SDL_Renderer * renderer);
+void SDL_RenderPresent(TH(main), SDL_Renderer * renderer);
 
-extern DECLSPEC void SDLCALL SDL_RenderClear(TH(main), SDL_Renderer * renderer);
+void SDL_RenderClear(TH(main), SDL_Renderer * renderer);
 
-extern DECLSPEC int SDLCALL SDL_RenderCopy(
+int SDL_RenderCopy(
     TH(main),
     SDL_Renderer * renderer, SDL_Texture * texture,
     const SDL_Rect * srcrect, const SDL_Rect * dstrect);
